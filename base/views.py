7a83ef9ebfc30dcffeaff834d1244f3d282c26b4
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-04-23 21:48:39 +0600

    added dyamic routing and query

--- base/views.py
+++ base/views.py
@@ -1,8 +1,24 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
-        return render(request, 'home.html')
+# Create Your Views Here
 
-def room(request):
-        return render(request, 'room.html')
+rooms = [
+        {'id':1,'name':'Lets Learn Python'},
+        {'id':2,'name':'Design With Me'},
+        {'id':3,'name':'Frontend Developers'},
+]
+
+def home(request):
+        context = {'rooms':rooms}
+        return render(request, 'base/home.html', context)
+
+def room(request, pk):
+        room = None
+        for i in rooms:
+                if str(i['id']) == str(pk):
+                        room = i
+                        break
+        context = {'room':room}
+
+        return render(request, 'base/room.html', context)
