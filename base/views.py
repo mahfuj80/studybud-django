b9cc83817bf344749afd6d663e354cd92095c654
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-04-28 01:44:41 +0600

    flash messages and login user authenticate created

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from .models import Room, Topic
 from .forms import RoomForm
@@ -26,11 +27,17 @@ def loginPage(request):
                 except :
                         messages.error(request, 'User Does Not Exist')
                         return redirect('login')
+                
+                user = authenticate(request, username=username, password=password)
 
+                if user is not None:
+                        login(request, user)
+                        return redirect('home')
+                else:
+                        messages.error(request, 'Invalid Credentials')
+                        return redirect('login')
         context = {}
         return render(request, 'base/login_register.html', context)
-
-
 
 def home(request):
         q = request.GET.get('q') if request.GET.get('q') != None else ''
