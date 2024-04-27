b0acf96258d27053fa28d1b05e0a026cd9752440
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-04-28 01:15:59 +0600

    Added Search Implementation

--- base/views.py
+++ base/views.py
@@ -1,21 +1,28 @@
 # from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 
 # Create Your Views Here
 
-rooms = [
-        {'id':1,'name':'Lets Learn Python'},
-        {'id':2,'name':'Design With Me'},
-        {'id':3,'name':'Frontend Developers'},
-]
+# rooms = [
+#         {'id':1,'name':'Lets Learn Python'},
+#         {'id':2,'name':'Design With Me'},
+#         {'id':3,'name':'Frontend Developers'},
+# ]
 
 def home(request):
-        rooms = Room.objects.all()
-        print(rooms)
-        context = {'rooms':rooms}
+        q = request.GET.get('q') if request.GET.get('q') != None else ''
+        rooms = Room.objects.filter(
+                Q(topic__name__icontains=q) |
+                Q(name__icontains=q) |
+                Q(description__icontains=q) 
+                )
+        topics = Topic.objects.all()
+        room_count = rooms.count()
+        context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
         return render(request, 'base/home.html', context)
 
 def room(request, pk):
