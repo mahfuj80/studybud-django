ca67ed0eb8d658f8f839217c6dddf52f3a33aba7
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-04-28 20:53:03 +0600

    Added Logout Method

--- base/views.py
+++ base/views.py
@@ -39,6 +39,14 @@ def loginPage(request):
         context = {}
         return render(request, 'base/login_register.html', context)
 
+
+def logoutUser(request):
+        logout(request)
+        return redirect('home')
+
+
+
+
 def home(request):
         q = request.GET.get('q') if request.GET.get('q') != None else ''
         rooms = Room.objects.filter(
