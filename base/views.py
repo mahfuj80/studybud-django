cfa4f7505038e8c29f28b5288802a4d674634ebc
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-07-13 00:15:54 +0600

    updated login

--- base/views.py
+++ base/views.py
@@ -24,6 +24,32 @@ from django.contrib.auth.forms import UserCreationForm
 # ]
 
 
+def loginPage(request):
+        page = 'login'
+        if request.user.is_authenticated:
+                return redirect('home')
+        
+        if request.method == 'POST':
+                username = request.POST.get('username').lower()
+                password = request.POST.get('password')
+
+                try:
+                        user = User.objects.get(username=username)
+                except :
+                        messages.error(request, 'User Does Not Exist')
+                        return redirect('login')
+                
+                user = authenticate(request, username=username, password=password)
+
+                if user is not None:
+                        login(request, user)
+                        return redirect('home')
+                else:
+                        messages.error(request, 'Invalid Credentials')
+                        return redirect('login')
+        context = {'page' : page}
+        return render(request, 'base/login_register.html', context)
+
 
 def logoutUser(request):
         logout(request)
