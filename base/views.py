5e1705904b8863dd3979ca8271cc5f40768bb7fc
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-08-13 00:35:04 +0600

    updated update profile and delete template

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -180,4 +180,12 @@ def deleteMessage(request, pk):
 
 @login_required(login_url='login')
 def updateUser(request):
-        return render(request, 'base/update-user.html')
+        user = request.user
+        form = UserForm(instance=user)
+        
+        if request.method == 'POST':
+                form = UserForm(request.POST, instance=user)
+                if form.is_valid():
+                        form.save()
+                        return redirect('user-profile', pk=user.id)
+        return render(request, 'base/update-user.html', {'form': form})
